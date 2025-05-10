159e5c291a8304f165cd051800a6cd8b1b827758
Whon1980-DL <supitpongw@hotmail.com>
2025-05-10 20:30:50 +0100

    Add basic checkout functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -6,9 +6,9 @@
     https://stripe.com/docs/stripe-js
 */
 
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     base: {
@@ -27,3 +27,55 @@ var style = {
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+//Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" roles="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `
+        $(errorDiv).html(html);
+    } else {
+        error.Div.textContent = '';
+    }
+})
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+        ev.preventDefault();
+        card.update({ 'disable': true});
+        $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" roles="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disable': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            // The payment has been processed!
+            if (result.paymentIntent.status === 'succeeded') {
+            // Show a success message to your customer
+            // There's a risk of the customer closing the window before callback
+            // excution. Set up a webhook or plugin to listen for the 
+            // payment_intent.succeeded event that handles any business critical
+            // post-payment actions.
+                form.submit();
+            }  
+        }
+    });
+});
